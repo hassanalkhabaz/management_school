704a2fd5442c6fb098fdb19d3ff427418628f07b
sharloo12 <dashmetal.12@gmail.com>
2021-06-05 00:03:35 +0200

    add homework fields added

--- lib/ui/pages/add_homeword.dart
+++ lib/ui/pages/add_homeword.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:management_school/ui/widgets/DropDownField.dart';
 import 'package:management_school/ui/widgets/MyDrawer.dart';
+import 'package:management_school/ui/widgets/fields.dart';
 
 class AddHomeWork extends StatefulWidget {
   @override
@@ -21,7 +23,28 @@ class _AddHomeWorkState extends State<AddHomeWork> {
           child: Padding(
             padding: const EdgeInsets.all(20.0),
             child: Column(
-              children: [],
+              children: [
+                FormBuilderDateTimePicker(
+                  name: 'date',
+                  inputType: InputType.date,
+                  onChanged: (value) {},
+                  decoration: InputDecoration(hintText: 'Submitting Date'),
+                  initialDate: DateTime.now(),
+                ),
+                SizedBox(height: 20),
+                DropDownField(
+                  onChange: (val) {},
+                  items: ['math', 'arabic'],
+                ),
+                SizedBox(height: 20),
+                textField(
+                  context,
+                  label: "Description",
+                  name: "description",
+                  hint: "add homework description",
+                  onChanged: (str) {},
+                ),
+              ],
             ),
           ),
         ),
