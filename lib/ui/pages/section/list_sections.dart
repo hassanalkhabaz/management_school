1371e71a1d42f585a770eb6fba9cfdcb71e71523
laolao <leenallaf98@gmail.com>
2021-06-27 18:33:58 +0300

    typo fixed

--- lib/ui/pages/section/list_sections.dart
+++ lib/ui/pages/section/list_sections.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:management_school/api/api_helper.dart';
 import 'package:management_school/model/section_model.dart';
+import 'package:management_school/ui/widgets/DropDownField.dart';
 import 'package:management_school/ui/widgets/MyDrawer.dart';
 
 class ListSections extends StatefulWidget {
@@ -11,6 +13,7 @@ class ListSections extends StatefulWidget {
 class _ListSectionsState extends State<ListSections> {
   List<SectionModel> sectionsData;
   bool _isLoading = true;
+  final _formKey = GlobalKey<FormBuilderState>();
 
   @override
   void initState() {
@@ -24,6 +27,7 @@ class _ListSectionsState extends State<ListSections> {
       appBar: AppBar(
         title: Text('All Sections'),
         backgroundColor: Colors.cyan,
+        bottom: buildDropDwonSelectionField(),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -65,6 +69,36 @@ class _ListSectionsState extends State<ListSections> {
       onTap: onTap,
       title: Text(name),
       // trailing: Icon(Icons.arrow_forward_ios),
+    );
+  }
+
+  PreferredSize buildDropDwonSelectionField() {
+    double verticalSpacing = 40;
+    double horizontalPadding = 10;
+    double verticalPadding = 10;
+    return PreferredSize(
+      preferredSize: Size(0, verticalSpacing * (1.5)),
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: horizontalPadding,
+          vertical: verticalPadding,
+        ),
+        child: Column(children: <Widget>[
+          FormBuilder(
+            key: _formKey,
+            child: DropDownField(
+              //TODO:init values
+              hint: 'Select Class',
+              items: ['class1', 'class2', 'class3'],
+              onChange: (val) {
+                setState(() {
+                  // logic
+                });
+              },
+            ),
+          )
+        ]),
+      ),
     );
   }
 
